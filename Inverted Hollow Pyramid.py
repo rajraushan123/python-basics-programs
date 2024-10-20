cbe5ef1ea09386d77d429dd25426989a051f0f30
Raushan Raj <148775493+rajraushan123@users.noreply.github.com>
2024-10-20 19:15:20 +0530

    Add files via upload

---
+++ Inverted Hollow Pyramid.py
@@ -0,0 +1,9 @@
+def inverted_hollow_pyramid(rows):
+    for i in range(rows, 0, -1):
+        if i == rows:
+            print('*' * (2 * i - 1))
+        else:
+            print(' ' * (rows - i) + '*' + ' ' * (2 * i - 3) + ('*' if i > 1 else ''))
+
+n = int(input("Enter the number of rows: "))
+inverted_hollow_pyramid(n)
